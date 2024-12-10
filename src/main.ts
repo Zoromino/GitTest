d4e577f5b5db5e3652d0e063c9e3e04d290638d9
dianaharoshka <dianagoroshko97@gmail.com>
2024-12-10 12:44:56 +0100

    comment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import "./style.css";
-<<<<<<< HEAD
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-=======
->>>>>>> 518f80d8bcc15024ac66e97b2a250fc7b45b90d1
 
 // Sakarias was here
 // Sakarias was here again
